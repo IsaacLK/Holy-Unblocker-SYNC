9dc4b1f8a4f4cb77af4e32119ec804bc83f45da0
IsaacLK <50344947+IsaacLK@users.noreply.github.com>
2022-06-07 18:37:38 -0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-cdr= os.getcwd()
-
-print(cdr)
-os.system("git clone https://git.holy.how/holy/website transfers/")
-print("mv -v "+str(cdr)+"/transfers/*"+" "+ str(cdr)+"/" )
-os.system("mv -v "+str(cdr)+"/transfers/*"+" "+ str(cdr)+"/" )
-os.system("yes | rm -r  "+ str(cdr)+"/transfers/")
